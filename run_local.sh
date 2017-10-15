55a4dbbb15642e9432fd86e3449ea79ac0ba40f4
noknod <yurij.klyushkin@pressindex.ru>
2017-10-15 19:19:10 +0500

    2

--- run_local.sh
+++ run_local.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 OUT_DIR=out
-NUM_REDUCERS=0 # > 0 to run the Reduce phase
+NUM_REDUCERS=1 # > 0 to run the Reduce phase
 CONFIG="--config /home/agorokhov/conf.empty"
 
 hdfs ${CONFIG} dfs -rm -r -skipTrash ${OUT_DIR}
@@ -9,11 +9,11 @@ hdfs ${CONFIG} dfs -rm -r -skipTrash ${OUT_DIR}
 yarn ${CONFIG} jar /opt/cloudera/parcels/CDH/lib/hadoop-mapreduce/hadoop-streaming.jar \
     -D mapreduce.job.name="Uniq users step1" \
     -D mapreduce.job.reduces=$NUM_REDUCERS \
-    -files tr.py \
-    -mapper "./tr.py | grep 193.227.136.104" \
+    -files metric01_1_mapper.py, metric01_1_reducer.py \
+    -mapper "./metric01_1_mapper.py" \
+    -reducer "./metric01_1_reducer.py" \
     -input in \
     -output ${OUT_DIR}
-    #-reducer TODO \
 
 # TODO:
 # - run in distributed mode (on the cluster)
